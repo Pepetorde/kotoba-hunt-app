792fa76b8bc75a3f847d6aa65640847025dd2f1e
Pepetorde <pepetorde@gmail.com>
2026-04-15 23:29:27 +0900

    chore: Create empty Xcode project

---
+++ KotobaHunt/KotobaHunt/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  KotobaHunt
+//
+//  Created by Pepe on 15/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Kotoba Hunt")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KotobaHunt/KotobaHunt/KotobaHuntApp.swift
@@ -0,0 +1,18 @@
+//
+//  KotobaHuntApp.swift
+//  KotobaHunt
+//
+//  Created by Pepe on 15/4/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct KotobaHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
